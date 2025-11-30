8d78b7bd7ab0899fa64a97543e6ea0dea5db4ef5
MarcoLvr <marco.lvr@outlook.it>
2025-11-30 19:54:55 +0100

    fatta sezione news. Come al solito da fare il responsive. Aggiunto lo smooth scroll per rendere tutto più juicy. Cambiata descrizione evento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,21 +9,23 @@ export default function Home() {
     <div>
         <Header />
         <div>
-            <div className={css.banner}>
-                <h1 className={css.bannerTitle}>RISE FROM THE ASHES</h1>
-                <h1 className={css.bannerCountdown}><Countdown target={new Date('2026-06-05')} /></h1>
-            </div>
-            <div className={css.video}>
-                <iframe
-                    src={`https://stream.hydromcstudios.com?path=sciencenight/banner/banner.m3u8&muted=1&nocontrols=1&loop=1`}
-                    style={{
-                        width: "100%",
-                        height: "100%",
-                        border: "none",
-                    }}
-                    //allow="autoplay"
-                    allowFullScreen
-                />
+            <div className={css.bannerContainer}>
+                <div className={css.bannerText}>
+                    <h1 className={css.bannerTitle}>RISE FROM THE ASHES</h1>
+                    <h1 className={css.bannerCountdown}><Countdown target={new Date('2026-06-05')} /></h1>
+                </div>
+                <div className={css.bannerVideo}>
+                    <iframe
+                        src={`https://stream.hydromcstudios.com?path=sciencenight/banner/banner.m3u8&muted=1&nocontrols=1&loop=1`}
+                        style={{
+                            width: "100%",
+                            height: "100%",
+                            border: "none",
+                        }}
+                        //allow="autoplay"
+                        allowFullScreen
+                    />
+                </div>
             </div>
             <div className={css.bannerShadowContainer}>
                 <Image className={css.bannerShadow} src={"/img/shadow.png"} width={1920} height={200} unoptimized alt={"shadow"}/>
@@ -39,11 +41,10 @@ export default function Home() {
                         <h1>RISE FROM THE ASHES</h1>
                     </div>
                     <div className={css.newYearDesc}>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                            It has survived not only five centuries, but also the leap into electronic typesetting,
-                            remaining essentially unchanged.</p>
+                        <p>"Ora voi o risorgete come squadra o cederete un centimetro alla volta, uno stand dopo l'altro fino alla disfatta".
+                        Queste sono le parole di chi, fino al 2017, ha sempre cercato di supportare l'evento prima che l'organizzazione crollasse nell'oblio.
+                        Ma le cose stanno cambiando. Il momento della rinascita di Festa scienze, ribrandizzata in Science Night, è giunto e ora più che mai la nuova organizzazione si sta preparando
+                        a far risplendere l'evento più rinnomato dell'Università di Verona. Tic Tac, il tempo scorre. Preparati alla nuova edizione di Science Night!</p>
                     </div>
                     <div>
                         <a href="#" className="button-glow">
@@ -53,13 +54,48 @@ export default function Home() {
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <div>
-                        <h1>Le ultime novità</h1>
-                    </div>
-                    <div>
-
+            <div className={css.newsSection}>
+                <div className={css.newsTitle}>
+                    <span>Le ultime novità</span>
+                </div>
+                <div className={css.newsContainer}>
+                    <div className={css.news}>
+                        <div className={css.newsElement}>
+                            <div className={css.newsTag}>
+                                <span>FOTO 2017</span>
+                            </div>
+                            <div className={css.newsImage}>
+                                <div className={css.newsImageOverlay}></div>
+                                <img src={"/img/testimg.png"}/>
+                            </div>
+                        </div>
+                        <div className={css.newsElement}>
+                            <div className={css.newsTag}>
+                                <span>FOTO 2017</span>
+                            </div>
+                            <div className={css.newsImage}>
+                                <div className={css.newsImageOverlay}></div>
+                                <img src={"/img/testimg.png"}/>
+                            </div>
+                        </div>
+                        <div className={css.newsElement}>
+                            <div className={css.newsTag}>
+                                <span>FOTO 2017</span>
+                            </div>
+                            <div className={css.newsImage}>
+                                <div className={css.newsImageOverlay}></div>
+                                <img src={"/img/testimg.png"}/>
+                            </div>
+                        </div>
+                        <div className={css.newsElement}>
+                            <div className={css.newsTag}>
+                                <span>FOTO 2017</span>
+                            </div>
+                            <div className={css.newsImage}>
+                                <div className={css.newsImageOverlay}></div>
+                                <img src={"/img/testimg.png"}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
