7cbf32602489826ab9cbedad89c7eaee3cf0a62b
MarcoLvr <marco.lvr@outlook.it>
2025-12-19 20:16:30 +0100

    lavorando al menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,25 @@
 import css from './header.module.css'
 
+import Menu from './menu';
+
 export default function Header(){
     return (
-        <header className={css.header}>
-            <div className={css.logo}>
-                <button><img src={"img/logo.svg"} alt="logo" /></button>
-            </div>
-            <div className={css.headerTitle}>
-                <h2>Science Night</h2>
-            </div>
-            <div className={css.menu}>
-                <button>
-                    <img src={"img/burger.svg"} alt="menu" />
-                </button>
-            </div>
-        </header>
+        <div>
+            <header className={css.header}>
+                <div className={css.logo}>
+                    <button><img src={"img/logo.svg"} alt="logo" /></button>
+                </div>
+                <div className={css.headerTitle}>
+                    <h2>Science Night</h2>
+                </div>
+                <div className={css.menu}>
+                    <button>
+                        <img src={"img/burger.svg"} alt="menu" />
+                    </button>
+                </div>
+            </header>
+            <Menu />
+        </div>
+
     )
 }
---
+++ src/components/menu.tsx
@@ -0,0 +1,38 @@
+import css from './menu.module.css'
+
+export default function Menu(){
+    return (
+        <div className={css.menu}>
+            <div className={css.menuContainer}>
+                <div className={css.menuList}>
+                    <h2>SCIENCE NIGHT 2026</h2>
+                    <div className={css.menuEntries}>
+                        <a href={"/"}>Il tema</a>
+                        <a href={"/"}>Prevendite</a>
+                    </div>
+                </div>
+                <div className={css.menuList}>
+                    <h2>NEWS</h2>
+                    <div className={css.menuEntries}>
+                        <a href={"/"}>Il tema</a>
+                        <a href={"/"}>Prevendite</a>
+                    </div>
+                </div>
+                <div className={css.menuList}>
+                    <h2>SCOPRI SCIENCE NIGHT</h2>
+                    <div className={css.menuEntries}>
+                        <a href={"/"}>La storia dell'evento</a>
+                        <a href={"/"}>Foto delle edizioni precedenti</a>
+                    </div>
+                </div>
+                <div className={css.menuList}>
+                    <h2>COLLABORA</h2>
+                    <div className={css.menuEntries}>
+                        <a href={"/"}>Diventa partner</a>
+                        <a href={"/"}>Entra nella crew</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
