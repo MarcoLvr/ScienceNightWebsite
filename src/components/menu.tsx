6e9cbfb6691d73c2eaa2110b5c5fb4892a4ef537
MarcoLvr <marco.lvr@outlook.it>
2026-02-03 17:36:59 +0100

    fatto menu :)

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -8,29 +8,29 @@ export default function Menu(){
                 <div className={css.menuList}>
                     <h2>SCIENCE NIGHT 2026</h2>
                     <div className={css.menuEntries}>
-                        <a href={"/"}>Il tema</a>
-                        <a href={"/"}>Prevendite</a>
+                        <a href={"/comingsoon"}>Il tema</a>
+                        <a href={"/comingsoon"}>Prevendite</a>
                     </div>
                 </div>
                 <div className={css.menuList}>
                     <h2>NEWS</h2>
                     <div className={css.menuEntries}>
-                        <a href={"/"}>Il tema</a>
-                        <a href={"/"}>Prevendite</a>
+                        <a href={"/comingsoon"}>Sito rinnovato</a>
+                        <a href={"https://www.youtube.com/watch?v=IHaE2CYc1n8"}>Debug Homecoming Aftermovie</a>
                     </div>
                 </div>
                 <div className={css.menuList}>
                     <h2>SCOPRI SCIENCE NIGHT</h2>
                     <div className={css.menuEntries}>
-                        <a href={"/"}>La storia dell'evento</a>
-                        <a href={"/"}>Foto delle edizioni precedenti</a>
+                        <a href={"/comingsoon"}>La storia dell'evento</a>
+                        <a href={"/comingsoon"}>Foto delle edizioni precedenti</a>
                     </div>
                 </div>
                 <div className={css.menuList}>
                     <h2>COLLABORA</h2>
                     <div className={css.menuEntries}>
-                        <a href={"/"}>Diventa partner</a>
-                        <a href={"/"}>Entra nella crew</a>
+                        <a href={"/comingsoon"}>Diventa partner</a>
+                        <a href={"/comingsoon"}>Entra nella crew</a>
                     </div>
                 </div>
             </div>
